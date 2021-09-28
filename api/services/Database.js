09987d0ef62eacbb67f58e1dcbe2c7a6179a7cdd
Gabriel Refosco <gbrefosco@gmail.com>
2021-09-28 01:54:37 -0300

    altera conexão com banco de dados

--- api/services/Database.js
+++ api/services/Database.js
@@ -1,12 +1,23 @@
 const Pool = require('pg').Pool;
+
+//URL: postgres://cnpbpkzq:Eh8YFaBVu-K-dQJqTxbIro5oBZPCnR1i@motty.db.elephantsql.com/cnpbpkzq
 const pool = new Pool({
-    user: 'iotpchjiewrrra',
-    host: 'ec2-3-218-47-9.compute-1.amazonaws.com',
-    database: 'ds1v5ecvlo8rl',
-    password: 'c7402a540bf1f05c4994e06b8cacd9a2ca8d815f12a24303fb946586641b2fc5',
+    url: 'postgres://cnpbpkzq:Eh8YFaBVu-K-dQJqTxbIro5oBZPCnR1i@motty.db.elephantsql.com/cnpbpkzq',
+    user: 'cnpbpkzq',
+    host: 'motty.db.elephantsql.com',
+    database: 'cnpbpkzq',
+    password: 'Eh8YFaBVu-K-dQJqTxbIro5oBZPCnR1i',
     port: 5432,
-    //CLI Connect: heroku pg:psql postgresql-colorful-39704 --app api-node-gbr
+    api_key: 'afad2479-e131-4cc7-a4e6-3990f386983e'
 });
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'postgres',
+//     password: '',
+//     port: 5432,
+// });
 
 async function run(query) {
     return pool.query(query);
